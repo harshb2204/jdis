6e70120787ce754451f34131371628a0e7888ce1
Harsh <harshb2204@gmail.com>
2026-07-01 16:05:34 +0530

    feat: update configuration for key limits and eviction strategy

--- jdis/src/main/java/com/jdis/config/Config.java
+++ jdis/src/main/java/com/jdis/config/Config.java
@@ -3,7 +3,12 @@ package com.jdis.config;
 public class Config {
     public static String HOST = "0.0.0.0";
     public static int PORT = 7379;
-    public static int KEYS_LIMIT = 5;
-    public static String EVICTION_STRATEGY = "simple-first";
+
+    public static int KEYS_LIMIT = 100;
+
+    /** Will evict EVICTION_RATIO of keys whenever eviction runs. */
+    public static double EVICTION_RATIO = 0.40;
+
+    public static String EVICTION_STRATEGY = "allkeys-lru";
     public static String AOF_FILE = "./jdis-master.aof";
 }
